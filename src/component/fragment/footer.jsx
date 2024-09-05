4343f2f31ce594a96475eee66481049d0bd03586
Alphby48 <hazerdmobile@gmail.com>
2024-09-05 23:47:36 +0700

    ending

--- src/component/fragment/footer.jsx
+++ src/component/fragment/footer.jsx
@@ -1,26 +1,35 @@
+import { useContext } from "react";
+import { DarkMode } from "../../context/darkModeContext";
 const FooterPage = () => {
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
   return (
-    <footer className="w-full py-14 bg-sky-300">
+    <footer
+      className={`w-full py-14 ${isDarkMode ? "bg-slate-400" : "bg-sky-300"}`}
+    >
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto">
-          <div className="flex space-x-10 justify-center items-center mb-14">
-            <a href="">
+          <div className="flex space-x-10 justify-center items-center mb-10">
+            <a href="https://www.instagram.com/denzuko_743">
               <i className="fa-brands fa-instagram text-3xl"></i>
             </a>
-            <a href="">
+            <a href="https://www.tiktok.com/@denzuko743">
               <i className="fa-brands fa-tiktok text-3xl"></i>
             </a>
-            <a href="">
+            <a href="https://x.com/denzuko_743">
               <i className="fa-brands fa-x-twitter text-3xl"></i>
             </a>
-            <a href="">
+            <a href="https://www.threads.net/@denzuko_743">
               <i className="fa-brands fa-threads text-3xl"></i>
             </a>
-            <a href="">
+            <a href="https://github.com/Alphby48">
               <i className="fa-brands fa-github text-3xl"></i>
             </a>
           </div>
-          <span className="text-lg text-gray-500 text-center block font-poppins">
+          <span
+            className={`text-lg text-center block font-poppins ${
+              isDarkMode ? "text-white" : "text-gray-600"
+            }`}
+          >
             ©Aditya 2024, All rights reserved.
           </span>
         </div>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -35,87 +35,91 @@ const ContactPage = () => {
   return (
     <div className="w-full flex items-center flex-col min-h-screen">
       <div className="w-full flex justify-center mb-8">
-        <h1
-          className={`relative text-3xl max-sm:mb-10 ${
-            isDarkMode ? "text-white" : "text-black"
-          } font-poppins font-semibold text-shadow-md after:absolute after:-bottom-2 after:left-0 after:w-20 after:h-1 after:bg-amber-400`}
-        >
-          Contact
-        </h1>
+        <Fade left>
+          <h1
+            className={`relative text-3xl max-sm:mb-10 ${
+              isDarkMode ? "text-white" : "text-black"
+            } font-poppins font-semibold text-shadow-md after:absolute after:-bottom-2 after:left-0 after:w-20 after:h-1 after:bg-amber-400`}
+          >
+            Contact
+          </h1>
+        </Fade>
       </div>
-      <form
-        ref={form}
-        onSubmit={sendEmail}
-        action=""
-        className={`flex flex-col w-2/3 max-sm:w-full p-5 shadow-neor ${
-          isDarkMode ? "bg-slate-500" : "bg-lime-200"
-        }`}
-      >
-        <div className="flex flex-col">
-          <label
-            htmlFor="user_name"
-            className={`text-xl font-poppins mb-2 ${
-              isDarkMode ? "text-white" : "text-black"
-            }`}
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="user_name"
-            required
-            className={`p-2 text-base font-poppins rounded-md mb-2 ${
-              isDarkMode ? "bg-slate-300" : "bg-slate-100"
-            }`}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="user_email"
-            className={`text-xl font-poppins mb-2 ${
-              isDarkMode ? "text-white" : "text-black"
-            }`}
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="user_email"
-            required
-            className={`p-2 text-base font-poppins rounded-md mb-2 ${
-              isDarkMode ? "bg-slate-300" : "bg-slate-100"
-            }`}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="message"
-            className={`text-xl font-poppins mb-2 ${
-              isDarkMode ? "text-white" : "text-black"
-            }`}
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className={`p-2 text-base font-poppins rounded-md mb-2 ${
-              isDarkMode ? "bg-slate-300" : "bg-slate-100"
-            }`}
-          ></textarea>
-        </div>
-        <div className="flex justify-center my-10">
-          <button
-            type="submit"
-            value="Send"
-            className="p-2 text-base font-poppins rounded-md bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+      <Zoom>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          action=""
+          className={`flex flex-col w-2/3 max-sm:w-full p-5 shadow-neor ${
+            isDarkMode ? "bg-slate-500" : "bg-lime-200"
+          }`}
+        >
+          <div className="flex flex-col">
+            <label
+              htmlFor="user_name"
+              className={`text-xl font-poppins mb-2 ${
+                isDarkMode ? "text-white" : "text-black"
+              }`}
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="user_name"
+              required
+              className={`p-2 text-base font-poppins rounded-md mb-2 ${
+                isDarkMode ? "bg-slate-300" : "bg-slate-100"
+              }`}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="user_email"
+              className={`text-xl font-poppins mb-2 ${
+                isDarkMode ? "text-white" : "text-black"
+              }`}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="user_email"
+              required
+              className={`p-2 text-base font-poppins rounded-md mb-2 ${
+                isDarkMode ? "bg-slate-300" : "bg-slate-100"
+              }`}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="message"
+              className={`text-xl font-poppins mb-2 ${
+                isDarkMode ? "text-white" : "text-black"
+              }`}
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className={`p-2 text-base font-poppins rounded-md mb-2 ${
+                isDarkMode ? "bg-slate-300" : "bg-slate-100"
+              }`}
+            ></textarea>
+          </div>
+          <div className="flex justify-center my-10">
+            <button
+              type="submit"
+              value="Send"
+              className="p-2 text-base font-poppins rounded-md bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </Zoom>
       {popup && (
         <PopupLayout
           msg="Terima Kasih Sudah Menghubungi Kami!.."
